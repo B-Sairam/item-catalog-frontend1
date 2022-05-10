5b2405b998229dcc3304df845c49987e087e7a6b
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-05-10 17:02:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import AuthPage from './components/AuthPage';
+import ProtectedRoute from './ProtectedRoute';
+import Home from './components/Home';
+import Productpage from './components/Productpage';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import { DataState } from './context/Provider';
+import Order from './components/Order';
+import AdminPanel from './admin/AdminPanel';
+import Create from './admin/Create';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {setcartIteam}=DataState();
+  useEffect(()=>{
+    const data = JSON.parse(localStorage.getItem("cart"));
+    if(data ===null){
+
+    }else{
+      setcartIteam(data);
+      console.log(data);
+    }
+    // eslint-disable-next-line
+},[])
+
+  return <>
+   <Router>
+     <Routes>
+       <Route path='/' element={<AuthPage/>}/>
+      
+       <Route element={<ProtectedRoute/>}>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/admin' element={<AdminPanel/>}/>
+        <Route path='/product' element={<Productpage/>}/>
+        <Route path='/selected' element={<ProductDetail/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/create' element={<Create/>}/>
+       </Route>
+     </Routes>
+   </Router>
+  </>
+
 }
 
 export default App;
